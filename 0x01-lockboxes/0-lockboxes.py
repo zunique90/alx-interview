4e2d82d52cec35dad237d67b54cde6625ea6f743
charles ezebuike <ezebuike.charles0@gmail.com>
2023-07-05 18:47:54 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script for the method `canUnlockAll`"""
+
+
+def canUnlockAll(boxes):
+    """a method that determines if all the boxes can be opened"""
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
