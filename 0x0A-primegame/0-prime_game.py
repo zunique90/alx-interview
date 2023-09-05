363e0eace8040078e3532412bd1bad599a32924a
charles ezebuike <ezebuike.charles0@gmail.com>
2023-09-05 14:08:32 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,31 +6,29 @@ def isWinner(x, nums):
     """Determines the winner of each game"""
 
     def isPrime(num):
-        """checks if a number is a prime number"""
-        if num < 2:
+        if num <= 1:
             return False
         for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
                 return False
         return True
 
-    def canWin(num):
-        """condition for the winner"""
-        if num == 1:
-            return False
-        if num == 2 or num % 2 == 1:
-            return True
-        return False
+    def getPrimeCount(num):
+        count = 0
+        for i in range(2, num + 1):
+            if isPrime(i):
+                count += 1
+        return count
 
     maria_wins = 0
     ben_wins = 0
 
     for num in nums:
-        if canWin(num):
-            if isPrime(num):
-                maria_wins += 1
-            else:
-                ben_wins += 1
+        primeCount = getPrimeCount(num)
+        if primeCount % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
